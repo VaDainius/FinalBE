bb8dd4880fb6e251d8c1452f8664e0630fb50256
Dainius <dainius.vanagas@codeacademy.lt>
2023-04-25 19:11:55 +0300

    Pirmas commit. Parsisiustas Spring projektas

---
+++ src/main/java/ca/BaigiamasisBE/BaigiamasisBeApplication.java
@@ -0,0 +1,13 @@
+package ca.BaigiamasisBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaigiamasisBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaigiamasisBeApplication.class, args);
+	}
+
+}
